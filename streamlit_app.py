e034a9c506f808d480ed3ad8a6d613d3fe0ee518
sudhirsalunke3012 <109577587+sudhirsalunke3012@users.noreply.github.com>
2023-08-08 08:25:59 +0530

    Update streamlit_app.py
    added header and 3 lines of text menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Helathy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Racket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
