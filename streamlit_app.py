89339d766863f31a79ae2c008ed9980e395fc16e
sudhirsalunke3012 <109577587+sudhirsalunke3012@users.noreply.github.com>
2023-08-29 18:50:51 +0530

    Update streamlit_app.py
    added try/except to handle errors

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,18 +25,19 @@ streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # normalize fruity vice json response
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # create dataframe to display the normalized fruity vice response 
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
-#import requests
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) #commenting out
-
-# normalize fruity vice json response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# create dataframe to display the normalized fruity vice response 
-streamlit.dataframe(fruityvice_normalized)
 #Dont run anything past this line
 streamlit.stop()
 #import snowflake.connector
